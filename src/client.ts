dd032ba1d47231d3cb2f2abb4273c61e1f5120f0
PocketMiner82 <pocket.miner@gmx.de>
2025-11-25 19:41:04 +0100

    feat: add spotify web sdk

--- src/client.ts
+++ src/client.ts
@@ -38,3 +38,75 @@ conn.addEventListener("open", () => {
     conn.send("ping");
   }, 1000);
 });
+
+(document.getElementById("spotifyLogin") as HTMLElement).onclick = connect;
+
+const script = document.createElement('script');
+script.src = 'https://sdk.scdn.co/spotify-player.js';
+script.async = true;
+
+document.body.appendChild(script);
+
+function getCookie(name: string): string|null {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  // @ts-ignore
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
+
+async function getToken(redirect:boolean = true): Promise<string> {
+  let token = getCookie("spotify-access-token");
+
+  if (!token) {
+    const response = await fetch("/auth/refreshToken");
+    
+    if (response.ok) {
+      return getCookie("spotify-access-token") as string;
+    } else if (response.status == 401 && redirect) {
+      location.href = '/auth/login';
+    } else if (redirect) {
+      alert("Spotify authentication failed.");
+    }
+
+    return "";
+  }
+
+  return token;
+}
+
+let player: Spotify.Player;
+
+function connect() {
+  if (player) {
+    player.connect().then(success => {
+      if (success) {
+        (document.getElementById("spotifyLogin") as HTMLElement).hidden = true;
+
+        
+      } else {
+        alert("Spotify connection failed.");
+      }
+    });
+  }
+}
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+  player = new window.Spotify.Player({
+    name: "SongGuess",
+    getOAuthToken: cb => { getToken().then(token => cb(token)) },
+    volume: 0.15
+  });
+
+  if (getCookie("spotify-access-token")) {
+    connect();
+  } else {
+    getToken(false).then(token => {
+      if (token != "") {
+        connect();
+      } else {
+        (document.getElementById("spotifyLogin") as HTMLElement).hidden = false;
+      }
+    });
+  }
+};
